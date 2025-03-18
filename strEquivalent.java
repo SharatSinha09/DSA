bcdab4e1317dba73e886bb6c9fd2a65e43db5c6c
SharatSinha09 <sharat.sinha2002@gmail.com>
2025-03-19 05:13:45 +0530

    check if string arrays are equals or not

---
+++ strEquivalent.java
@@ -0,0 +1,19 @@
+public class strEquivalent {
+    public static void main(String[] args) {
+        String[] word1 = {"a", "bc"};
+        String[] word2 = {"ab", "c"};
+        boolean ans = arrayStringsAreEqual(word1, word2);
+        System.out.println(ans);
+    }
+    public static boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        StringBuilder w1 = new StringBuilder();
+        StringBuilder w2 = new StringBuilder();
+        for(int i=0; i<word1.length; i++){
+            w1.append(word1[i]);
+        }
+        for(int i=0; i<word2.length; i++){
+            w2.append(word2[i]);
+        }
+        return (w1.toString()).equals((w2.toString()));
+    }
+}

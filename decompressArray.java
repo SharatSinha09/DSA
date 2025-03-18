bfb110bc45b01e2fd53b81bf1974ff58c843b31f
SharatSinha09 <sharat.sinha2002@gmail.com>
2025-03-19 05:04:23 +0530

    decompress the array

---
+++ decompressArray.java
@@ -0,0 +1,25 @@
+public class decompressArray {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4};
+        int[] ans = decompressRLElist(nums);
+        for(int i : ans){
+            System.out.print(i + " ");
+        }
+    }
+    public static int[] decompressRLElist(int[] nums) {
+        int n = nums.length;
+        int len = 0;
+        for(int i=0; i<n; i+=2){
+            len+=nums[i];
+        }
+        int[] res = new int[len];
+        int idx = 0;
+        for(int i=0; i<n; i+=2){
+            int freq = nums[i];
+            int val = nums[i+1];
+            for(int j=0; j<freq; j++)
+            res[idx++] = val;
+        }
+        return res;
+    }
+}

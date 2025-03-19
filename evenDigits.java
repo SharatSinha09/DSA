1a68b6be40f194c024b127d3482e29aeda01f8d0
SharatSinha09 <sharat.sinha2002@gmail.com>
2025-03-20 04:48:10 +0530

    find numbers with even number of digits

---
+++ evenDigits.java
@@ -0,0 +1,23 @@
+public class evenDigits {
+    public static void main(String[] args) {
+        int[] nums = {12,345,2,6,7896};
+        System.out.println(findNumbers(nums));
+    }
+    public static int findNumbers(int[] nums) {
+        int even = 0;
+        for(int i=0; i<nums.length; i++){
+            if(countDigit(nums[i])%2 == 0){
+                even++;
+            }
+        }
+        return even;
+    }
+    public static int countDigit(int n) {
+        int count = 0;
+        while(n > 0){
+            count++;
+            n/=10;
+        }
+        return count;
+    }
+}

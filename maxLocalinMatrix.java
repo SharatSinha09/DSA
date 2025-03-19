e974a6c95fd99e8ca779d2bd0156c67861e870d6
SharatSinha09 <sharat.sinha2002@gmail.com>
2025-03-19 20:29:11 +0530

    longest local values in a matrix

---
+++ maxLocalinMatrix.java
@@ -0,0 +1,28 @@
+public class maxLocalinMatrix {
+    public static void main(String[] args) {
+        int[][] grid = {{9,9,8,1},{5,6,2,6},{8,2,6,4},{6,2,2,2}};
+        int[][] res = largestLocal(grid);
+        for(int i=0; i<res.length; i++){
+            for(int j=0; j<res.length; j++){
+                System.out.print(res[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    public static int[][] largestLocal(int[][] grid) {
+        int n = grid.length;
+        int[][] maxLocal = new int[n-2][n-2];
+        for(int i=0; i<n-2; i++){
+            for(int j=0; j<n-2; j++){
+                int max = 0;
+                for(int k=i; k<i+3; k++){
+                    for(int l=j; l<j+3; l++){
+                        max = Math.max(max, grid[k][l]);
+                    }
+                }
+                maxLocal[i][j] = max;
+            }
+        }
+        return maxLocal;
+    }
+}

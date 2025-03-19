b37f77c4f789d4ab7a76664a64cf9586f1d846b6
SharatSinha09 <sharat.sinha2002@gmail.com>
2025-03-19 20:08:00 +0530

    number of senior citizens

---
+++ countSerions.java
@@ -0,0 +1,16 @@
+public class countSerions {
+    public static void main(String[] args) {
+        String[] details = {"7868190130M7522","5303914400F9211","9273338290F4010"};
+        System.out.println(countSeniors(details));
+    }
+    public static int countSeniors(String[] details) {
+        int count = 0;
+        for(int i=0; i<details.length; i++){
+            String age = details[i].substring(11,13);
+            if(Integer.parseInt(age) > 60){
+                count++;
+            }
+        }
+        return count;
+    }
+}

9ab6111777ecb3fe1828b488b12e25957376c2cd
SharatSinha09 <sharat.sinha2002@gmail.com>
2025-04-10 19:55:13 +0530

    count negatives in a sorted matrix

--- countNegatives.java
+++ countNegatives.java
@@ -3,20 +3,23 @@ public class countNegatives {
         int[][] grid = {{4,3,2,-1},{3,2,1,-1},{1,1,-1,-2},{-1,-1,-2,-3}};
         System.out.println(countN(grid));
     }
-    public static int countN(int[][] grid){
-        int m = grid.length;
-        int n = grid[0].length;
-        int count = 0;
-        int i = m-1;
-        int j = 0;
-        while(i>=0 && j < n){
-            if(grid[i][j] >= 0){
-                j++;
-            }else{
-                count+=n-j;
-                i--;
+    
+    public static int countN(int[][] grid) {
+        int cnt = 0;
+        for(int i=0; i<grid.length; i++){
+            int l=0;
+            int h=grid[0].length-1;
+            while(l<=h){
+                int m = l+(h-l)/2;
+                if(grid[i][m] >= 0){
+                    l=m+1;
+                }
+                else{
+                    h=m-1;
+                }
             }
+            cnt += grid[0].length-l;
         }
-        return count;
+        return cnt;
     }
 }

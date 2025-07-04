1a1d2d9a74ec9f37cd424ed6fc9b8f978f90c678
SharatSinha09 <sharat.sinha2002@gmail.com>
2025-07-04 07:03:57 +0530

    sliding window

---
+++ minSizeSubArraySum.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class minSizeSubArraySum {
+    public static int minWindow(int n, int[] nums, int target) {
+        int i = 0;
+        int j = 0;
+        int minLen = Integer.MAX_VALUE;
+        int sum = 0;
+        while (j < n) {
+            sum += nums[j];
+            while (sum >= target) {
+                minLen = Math.min(minLen, j - 1 + 1);
+                sum -= nums[i++];
+            }
+            j++;
+        }
+        return minLen == Integer.MAX_VALUE ? 0 : minLen;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter size of array : ");
+        int n = Integer.parseInt(br.readLine());
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(br.readLine());
+        }
+        int target = Integer.parseInt(br.readLine());
+
+        System.out.println(minWindow(n, nums, target));
+    }
+}
